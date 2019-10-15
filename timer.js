8f08389af329ce49eaffb2c6cd15ee0692f102dc
anthony62490 <anthonyjmagno@gmail.com>
2019-10-15 00:45:32 -0500

    timing fix

--- timer.js
+++ timer.js
@@ -85,8 +85,8 @@ if(alert) {
   const randPhrase = `${phrase[Math.floor(Math.random()*phrase.length)]}`;
   const content = 
     (timeLeft.days > 0 || timeLeft.hours >= 20) ? `COUNTDOWN TO SPACE!\n${timeLeft.days} days, ${timeLeft.hours} hours, ${timeLeft.minutes} minutes, ${timeLeft.seconds} seconds` :
+    (timeLeft.days === 0 && timeLeft.hours === 0 && timeLeft.minutes <= 15) ? "@Space Truckers HOLY SHIT! HEY GUYS! IT'S TIME FOR COOL SPACE! GET YOUR ASSES IN HERE!" :
     (timeLeft.days === 0 && timeLeft.hours < 20 && timeLeft.hours > 0) ? `${randPhrase}\n${timeLeft.days} days, ${timeLeft.hours} hours, ${timeLeft.minutes} minutes, ${timeLeft.seconds} seconds` :
-    (timeLeft.days === 0 && timeLeft.hours === 0) ? "@Space Truckers HOLY SHIT! HEY GUYS! IT'S TIME FOR COOL SPACE! GET YOUR ASSES IN HERE!" :
     `Something has gone horribly wrong. I don't know what it is. ${JSON.stringify(debug)}`;
   const body = {
     username: "Space Clock",
@@ -100,7 +100,7 @@ if(alert) {
   };
 
   let id, token;
-  if (timeLeft.days === 0 && timeLeft.hours === 0) {
+  if (timeLeft.days === 0 && timeLeft.hours === 0 && timeLeft.minutes <= 15) {
     id = webhook2.id;
     token = webhook2.token;
   } else {
