de2398fb8086b16984ec84215854a51df7407f46
anthony62490 <anthonyjmagno@gmail.com>
2019-10-15 00:43:29 -0500

    swapping out phrases

--- timer.js
+++ timer.js
@@ -66,9 +66,6 @@ if(alert) {
   const phrase = [
     "Hey guys... GUYS! The space is coming! Be ready for it!",
     `Just a bit longer until ${person} dies! Yaaaay! \\o/`,
-    "Just a little bit longer. Almost time for space.",
-    "The maiden voyage of the Gift Horse is only THIS far off!",
-    "Tonight's the night! Let's show this universe who's BOSS!",
     "Someone warmed up the warp drive, right? We're gonna need that",
     "Can't wait for space!",
     "It's never too late to be a space punk band! We can do it!",
@@ -78,12 +75,12 @@ if(alert) {
     "Who's ready for space adventure?!",
     "Almost time for space!",
     "I'm gonna take enough space drugs to taste god!",
-    "On second thought guys, we should seriously swap out for the Patrol Boat hull.",
     `How long do you think before we need to shove ${person} out an airlock? I give us three days, tops.`,
     "On tonight's episode of Stars Without Number, the party buys enough guns to punch a hole in time. Don't miss it!",
     "Who's ready to get murdered by techno monks? I know I am!",
     "If anyone here tries to start a burgeoning shoe empire, you're going out the garbage chute.",
-    "Almost time to find out how many arms we can bolt on to the robot!"
+    "Almost time to find out how many arms we can bolt on to the robot!",
+    "At this rate, we should be the enemy of every pirate in known space by dinnertime!"  
   ];
   const randPhrase = `${phrase[Math.floor(Math.random()*phrase.length)]}`;
   const content = 
