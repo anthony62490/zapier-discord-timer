a95fd0227dc854ab561fd3318971b566a57747b4
anthony62490 <anthonyjmagno@gmail.com>
2019-10-15 01:07:06 -0500

    more phrases

--- timer.js
+++ timer.js
@@ -65,7 +65,6 @@ if(alert) {
   const person = party[Math.floor(Math.random()*party.length)];
   const phrase = [
     "Hey guys... GUYS! The space is coming! Be ready for it!",
-    `Just a bit longer until ${person} dies! Yaaaay! \\o/`,
     "Someone warmed up the warp drive, right? We're gonna need that",
     "Can't wait for space!",
     "It's never too late to be a space punk band! We can do it!",
@@ -74,13 +73,15 @@ if(alert) {
     `Just a little while until ${person} dies in space! Woooo! \\o/`,
     "Who's ready for space adventure?!",
     "Almost time for space!",
-    "I'm gonna take enough space drugs to taste god!",
+    "With all this money, I'm gonna take enough space drugs to taste god!",
     `How long do you think before we need to shove ${person} out an airlock? I give us three days, tops.`,
     "On tonight's episode of Stars Without Number, the party buys enough guns to punch a hole in time. Don't miss it!",
     "Who's ready to get murdered by techno monks? I know I am!",
     "If anyone here tries to start a burgeoning shoe empire, you're going out the garbage chute.",
     "Almost time to find out how many arms we can bolt on to the robot!",
-    "At this rate, we should be the enemy of every pirate in known space by dinnertime!"  
+    "At this rate, we should be the enemy of every pirate in known space by dinnertime!",
+    "AIs are people too! (but they still don't need money)",
+    "It has been [2] days since the crew vented someone into space."
   ];
   const randPhrase = `${phrase[Math.floor(Math.random()*phrase.length)]}`;
   const content = 
